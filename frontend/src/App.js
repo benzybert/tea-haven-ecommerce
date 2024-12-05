f30e2ec6a6fdf52e43666a6b66d74298dd47ce4f
Ben Roy Zybert <benzybert1@gmail.com>
2024-12-05 16:47:38 +0200

    Add Home and Products pages with routing in App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Products from './pages/Products';
 
 function App() {
   return (
@@ -8,7 +10,8 @@ function App() {
       <div className="min-h-screen">
         <Navbar />
         <Routes>
-          {/* Routes will be added here */}
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
         </Routes>
       </div>
     </Router>
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+  return <div className="container mx-auto p-4">Home Page</div>;
+};
+
+export default Home;
+
+// src/pages/Products.js
+import React from 'react';
+
+const Products = () => {
+  return <div className="container mx-auto p-4">Products Page</div>;
+};
+
+export default Products;
