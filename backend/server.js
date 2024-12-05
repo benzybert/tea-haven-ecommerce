195571d3136fbcb87bd8bb0ffa35394ff2bfecc2
Ben Roy Zybert <benzybert1@gmail.com>
2024-12-05 16:38:17 +0200

    Add initial files for various node modules and configuration

--- backend/server.js
+++ backend/server.js
@@ -19,5 +19,5 @@ mongoose.connect(process.env.MONGO_URI, {
 
 // Routes will be added here
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
