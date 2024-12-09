e9207c1dd946f5da0007fd7aef791d798201984f
ben roy zybert <96917127+benzybert@users.noreply.github.com>
2024-12-09 12:42:43 +0200

    Test pushing capability

---
+++ frontend/src/components/TestPush.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TestPush = () => {
+  return (
+    <div className="text-center p-4">
+      <h1>Test Push Successful!</h1>
+    </div>
+  );
+};
+
+export default TestPush;
